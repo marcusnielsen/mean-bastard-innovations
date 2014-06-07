ea8caa2c728fb8515ceac1753d610d9bdd16149d
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-06-07 16:27:56 +0200

    first html view

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 'use strict';
 
+var app = require("express")();
+var mongoose = require("mongoose");
+var swig = require("swig");
+
+app.engine('html', swig.renderFile);
+app.set('views', __dirname + '/app/views');
+
+app.get('/', function(req, res) {
+    res.render('index.server.html', {title: 'TODO'});
+});
+
+app.listen(3000);
