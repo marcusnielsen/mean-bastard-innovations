60332689f19f291c69b7f7c44dbb0c29a34b35dc
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-06-07 20:58:13 +0200

    added config

---
+++ config/env/all.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    app: {
+        title: 'MEAN Bastard Innovations',
+        description: 'Official webpage of Marcus Nielsen'
+    },
+    port: process.env.PORT || 3000,
+    db: 'mongodb://localhost/vanilla-webpage'
+};
--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 'use strict';
+var config = require('./config/env/all.js');
 
-var app = require("express")();
-var mongoose = require("mongoose");
-var swig = require("swig");
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var skillSchema = new Schema({
+    name: {
+        type: String,
+        default: '',
+        required: 'Name required',
+        trim: true
+    },
+    score: {
+      type: Number,
+      required: 'Score required',
+      min: 1,
+      max: 100
+    },
+    parent: {
+        type: Schema.ObjectId,
+        ref: 'Skill'
+    }
+});
+
+var Skill = mongoose.model('Skill', skillSchema);
+
+mongoose.connect(config.db);
+
+mongoose.connection.db.executeDbCommand({dropDatabase: 1}, function(err, result) {
+    if(err) {
+        console.log("drop database fail.");
+    }
+});
+
+var programmingSkill = new Skill({name: 'Programming', score: 70});
+programmingSkill.save(function(err) {
+    if(err)
+    {
+        console.log(err);
+    }
+});
+
+var meanSkill = new Skill({name: 'MEAN Stack', score: 20, parent: programmingSkill._id});
+meanSkill.save(function (err) {
+    if(err)
+    {
+        console.log(err);
+    }
+});
+
+Skill.find(function(err, skills) {
+    console.log(skills);
+});
+
+var app = require('express')();
+var swig = require('swig');
 
 app.engine('html', swig.renderFile);
 app.set('views', __dirname + '/app/views');
 
 app.get('/', function(req, res) {
-    res.render('index.server.html', {title: 'TODO'});
+    res.render('index.server.html', {title: config.app.title});
 });
 
 app.listen(3000);
