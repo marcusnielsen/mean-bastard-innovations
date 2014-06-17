9cce7eef88decb76369029f2f2001d619e5be1b3
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-06-18 00:16:50 +0200

    Fixed missing var.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var config = require('./app/config/env/all.js');
 var mongoose = require('mongoose');
 //TODO: Setup with MongoLabs when needed.
 // var db = mongoose.connect(config.db);
+var db = null;
 //TODO: require('../passport/passport.js')();
 var app = require('./app/express/express.js')(db, config);
 
