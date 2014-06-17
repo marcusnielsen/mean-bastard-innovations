71392e8c2e4fba5f245988a869c811e0bc1bd7e2
Marcus Nielsen <marcus.nielsen82@gmail.com>
2014-06-17 23:29:55 +0200

    Fixed random theme button.

--- public/app/components/theme/theme-factory.js
+++ public/app/components/theme/theme-factory.js
@@ -1,16 +1,24 @@
 'use strict';
 
 angular.module('app').factory('themeFactory', ['$http', function ($http) {
-    var themes = [];
+    var publicObj = {};
+
+    publicObj.selected = {
+        name: 'flatly',
+        cssCdn: '//netdna.bootstrapcdn.com/bootswatch/latest/flatly/bootstrap.min.css'
+    };
+
+    publicObj.themes = [];
 
     $http.get('//api.bootswatch.com/3/').success(function (data) {
-        themes = _.map(data.themes, function (theme) {
+        publicObj.themes = _.map(data.themes, function (theme) {
             return {name: theme.name, cssCdn: theme.cssCdn};
         });
     });
 
-    return {
-        selected: {name: 'flatly', cssCdn: '//netdna.bootstrapcdn.com/bootswatch/latest/flatly/bootstrap.min.css'},
-        themes: themes
+    publicObj.setRandomTheme = function () {
+        publicObj.selected = publicObj.themes[Math.floor(Math.random() * publicObj.themes.length)];
     };
+
+    return publicObj;
 }])
